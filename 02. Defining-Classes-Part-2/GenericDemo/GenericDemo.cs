2b126594c28d80225c10308468d2d66ece01e839
AyrFX <oleg@gbg.bg>
2016-06-12 23:39:33 +0300

    Added GenericDemo Project (tasks 5 and 6 of Defining classes Part I Homework)

---
+++ 02. Defining-Classes-Part-2/GenericDemo/GenericDemo.cs
@@ -0,0 +1,38 @@
+﻿namespace GenericDemo
+{
+    using System;
+
+    class GenericDemo
+    {
+        static void Main()
+        {
+            GenericList<int> list = new GenericList<int>();
+            list.AddItem(3);
+            list.AddItem(4);
+            list.AddItem(7);
+            list.AddItem(12);
+            list.AddItem(16);
+            list.AddItem(21);
+            Console.WriteLine(list.ToString());
+            Console.WriteLine(new string('=', 21));
+
+            list.RemoveElement(3);
+            Console.WriteLine(list.ToString());
+            Console.WriteLine(new string('=', 21));
+
+            Console.WriteLine(list.GetElement(4));
+            Console.WriteLine(new string('=', 21));
+
+            list.InsertElement(12, 3);
+            Console.WriteLine(list.ToString());
+            Console.WriteLine(new string('=', 21));
+
+            Console.WriteLine(list.GetIndex(4));
+            Console.WriteLine(new string('=', 21));
+
+            list.ClearList();
+            Console.WriteLine(list.ToString());
+            Console.WriteLine(new string('=', 21));
+        }
+    }
+}
---
+++ 02. Defining-Classes-Part-2/GenericDemo/GenericList.cs
@@ -0,0 +1,116 @@
+﻿namespace GenericDemo
+{
+    using System;
+
+    public class GenericList<T>
+    {
+        private T[] values;
+        private int lastIndex;
+        private const int InitialSize = 4;
+
+        public GenericList()
+        {
+            values = new T[InitialSize];
+            lastIndex = -1;
+        }
+
+        private void ResizeValuesArray()
+        {
+            if (this.lastIndex == this.values.Length - 1)
+            {
+                T[] newArray = new T[this.values.Length * 2];
+                this.values.CopyTo(newArray, 0);
+                this.values = newArray;
+            }
+        }
+
+        private void CheckIndex(int index)
+        {
+            if (0 > index || index > this.values.Length - 1)
+            {
+                throw new IndexOutOfRangeException("Given index is out of the range of the list!");
+            }
+        }
+
+        public void AddItem(T value)
+        {
+            ResizeValuesArray();
+            this.lastIndex++;
+            this.values[lastIndex] = value;
+        }
+
+        public T GetElement(int index)
+        {
+            CheckIndex(index);
+            return this.values[index];
+        }
+
+        public void RemoveElement(int index)
+        {
+            CheckIndex(index);
+            T[] newArray = new T[this.values.Length];
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = this.values[i];
+            }
+
+            for (int i = index+1; i < this.values.Length; i++)
+            {
+                newArray[i-1] = this.values[i];
+            }
+            this.values = newArray;
+            lastIndex--;
+        }
+
+        public void InsertElement(T value, int index)
+        {
+            CheckIndex(index);
+            ResizeValuesArray();
+            T[] newArray = new T[this.values.Length];
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = this.values[i];
+            }
+            newArray[index] = value;
+            for (int i = index+1; i < this.values.Length; i++)
+            {
+                newArray[i] = this.values[i - 1];
+            }
+            this.values = newArray;
+            lastIndex++;
+        }
+
+        public void ClearList()
+        {
+            T[] newArray = new T[InitialSize];
+            this.values = newArray;
+            lastIndex = -1;
+        }
+
+        public int GetIndex(T value)
+        {
+            for (int i = 0; i < this.values.Length; i++)
+            {
+                if (value.Equals(this.values[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public override string ToString()
+        {
+            string result = string.Empty;
+            for (int i = 0; i <= this.lastIndex; i++)
+            {
+                result += this.values[i];
+                if (i != this.lastIndex)
+                {
+                    result += ", ";
+                }
+            }
+            return result;
+        }
+    }
+}
