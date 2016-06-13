9b52f3a0183f1ce4a41f867cd6532939cffb4200
AyrFX <oleg@gbg.bg>
2016-06-13 23:17:13 +0300

    Added MatrixDemo (tasks 8 to 10) of Defining classes Part II Homework (but with not working operator +)

---
+++ 02. Defining-Classes-Part-2/MatrixDemo/Matrix.cs
@@ -0,0 +1,91 @@
+﻿namespace MatrixDemo
+{
+    using System;
+
+    public class Matrix<T>
+        where T : struct,
+          IComparable,
+          IComparable<T>,
+          IConvertible,
+          IEquatable<T>,
+          IFormattable
+    {
+        private T[,] matrix;
+
+        public Matrix(int rows, int cols)
+        {
+            this.matrix = new T[rows, cols];
+        }
+
+        private bool InMatrix(int row, int col)
+        {
+            if (row < 0 || row > this.matrix.GetLength(0) - 1 || col < 0 || col > this.matrix.GetLength(1) - 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public int GetLength(byte dimension)
+        {
+            if (dimension == 0)
+            {
+                return this.matrix.GetLength(0);
+            }
+            else if (dimension == 1)
+            {
+                return this.matrix.GetLength(1);
+            }
+            else
+            {
+                throw new ArgumentException("Given dimension is out of range!");
+            }
+        }
+
+        public T this[int row, int col]
+        {
+            get
+            {
+                if (InMatrix(row, col))
+                {
+                    return this.matrix[row, col];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("The given index is out of range of the matrix!");
+                }
+            }
+            set
+            {
+                if (InMatrix(row, col))
+                {
+                    this.matrix[row, col] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("The given index is out of range of the matrix!");
+                }
+            }
+        }
+
+        public static Matrix<T> operator +(Matrix<T> aNumber, Matrix<T> bNumber)
+        {
+            if (aNumber.GetLength(0) != bNumber.GetLength(0) || aNumber.GetLength(1) != bNumber.GetLength(1))
+            {
+                throw new ArgumentException("The matrices should be the same size!");
+            }
+            Matrix<T> newMatrix = new Matrix<T>(aNumber.GetLength(0), aNumber.GetLength(1));
+            for (int i = 0; i < newMatrix.GetLength(1); i++)
+            {
+                for (int j = 0; j < newMatrix.GetLength(1); j++)
+                {
+                    newMatrix[i, j] = aNumber[i, j] + bNumber[i, j];
+                }
+            }
+            return newMatrix;
+        }
+    }
+}
---
+++ 02. Defining-Classes-Part-2/MatrixDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace MatrixDemo
+{
+    using System;
+
+    class MatrixDemo
+    {
+        static void Main()
+        {
+            Matrix<int> someMatrix = new Matrix<int>(4, 4);
+            someMatrix[0, 0] = 3;
+            someMatrix[1, 1] = 3;
+            someMatrix[2, 2] = 3;
+            someMatrix[3, 3] = 3;
+
+            for (int i = 0; i < someMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < someMatrix.GetLength(1); j++)
+                {
+                    Console.Write(someMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(new String('=', 21));
+
+            Matrix<int> anotherMatrix = new Matrix<int>(4, 4);
+            someMatrix[0, 3] = 3;
+            someMatrix[1, 2] = 3;
+            someMatrix[2, 1] = 3;
+            someMatrix[3, 0] = 3;
+
+            for (int i = 0; i < someMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < someMatrix.GetLength(1); j++)
+                {
+                    Console.Write(someMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(new String('=', 21));
+
+            Matrix<int> addition = someMatrix + anotherMatrix;
+            for (int i = 0; i < someMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < someMatrix.GetLength(1); j++)
+                {
+                    Console.Write(someMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(new String('=', 21));
+        }
+
+    }
+}
