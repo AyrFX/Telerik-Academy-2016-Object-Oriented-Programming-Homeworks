fa0fe54f124b82cc3295b1b6ed0a92706db0e83b
AyrFX <oleg@gbg.bg>
2016-06-14 22:46:24 +0300

    Added AttributeDemo project to Defining classes part 2 homework

---
+++ 02. Defining-Classes-Part-2/AtributeDemo/AtributeDemo.cs
@@ -0,0 +1,18 @@
+﻿namespace AttributeDemo
+{
+    using System;
+
+    [VersionAttribute(3, 12)]
+    public class AttributeMain
+    {
+        public static void Main()
+        {
+            Type t = typeof(AttributeMain);
+            object[] allAttributes = t.GetCustomAttributes(false);
+            foreach (VersionAttribute attr in allAttributes)
+            {
+                Console.WriteLine("The class {0} is version {1}. ", attr.TypeId, attr.Version);
+            }
+        }
+    }
+}
---
+++ 02. Defining-Classes-Part-2/AtributeDemo/VersionAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace AttributeDemo
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Struct | AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Enum | AttributeTargets.Method, AllowMultiple = true)]
+    public class VersionAttribute : Attribute
+    {
+        private int major;
+        private int minor;
+
+        public VersionAttribute(int major, int minor)
+        {
+            this.major = major;
+            this.minor = minor;
+        }
+
+        public string Version
+        {
+            get
+            {
+                return string.Format(this.major + "." + this.minor);
+            }
+        }
+    }
+}
