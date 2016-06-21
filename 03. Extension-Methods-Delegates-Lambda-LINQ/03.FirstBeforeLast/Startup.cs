840a25c9d201ab5e1de5179a796bf385bdcbd93a
AyrFX <oleg@gbg.bg>
2016-06-21 23:36:02 +0300

    Added task 3 of Extension methods, delegates, Lambda and LINQ homework

---
+++ 03. Extension-Methods-Delegates-Lambda-LINQ/03.FirstBeforeLast/Startup.cs
@@ -0,0 +1,30 @@
+﻿namespace FirstBeforeLast
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        static void Main()
+        {
+            Student[] studentsList = new Student[] {
+            new Student("Pesho", "Goshov"),
+            new Student("Gosho", "Moshev"),
+            new Student("Veso", "Peshev"),
+            new Student("Tosho", "Moshev"),
+            new Student("Mosho", "Peshev"),
+            new Student("Pesho", "Vesev"),
+            new Student("Aaaa!", "Z")};
+
+            var selectedStudents =
+                from student in studentsList
+                where string.Compare(student.FristName, student.LastName, StringComparison.Ordinal) < 0
+                select student;
+
+            foreach (var student in selectedStudents)
+            {
+                Console.WriteLine("{0} {1}", student.FristName, student.LastName);
+            }
+        }
+    }
+}
---
+++ 03. Extension-Methods-Delegates-Lambda-LINQ/03.FirstBeforeLast/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstBeforeLast
+{
+    class Student
+    {
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+
+        public Student(string firstName, string lastName)
+        {
+            this.FristName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
