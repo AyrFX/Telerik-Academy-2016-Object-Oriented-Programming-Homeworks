ef4f73baf116cacad4c8f43125fc2e436ba895d5
AyrFX <oleg@gbg.bg>
2016-06-21 22:45:23 +0300

    added task 1 of Extension Methods, Delegates, Lambda and LINQ homework

---
+++ 03. Extension-Methods-Delegates-Lambda-LINQ/01.StringBuilderSubstring/Startup.cs
@@ -0,0 +1,17 @@
+﻿namespace StringBuilderSubstring
+{
+    using System;
+    using System.Text;
+
+    class Startup
+    {
+        static void Main()
+        {
+            int index = 3;
+            int count = 7;
+            StringBuilder someSB = new StringBuilder();
+            someSB.Append("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            Console.WriteLine(someSB.Substring(index, count));
+        }
+    }
+}
---
+++ 03. Extension-Methods-Delegates-Lambda-LINQ/01.StringBuilderSubstring/StringBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace StringBuilderSubstring
+{
+    using System.Text;
+
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder Substring(this StringBuilder input, int index, int count)
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append(input.ToString().Substring(index, count));
+            return result;
+        }
+    }
+}
