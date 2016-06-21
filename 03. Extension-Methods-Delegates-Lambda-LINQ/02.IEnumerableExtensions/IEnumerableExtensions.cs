dd305061216de7b51848c4b2a19e433544fdfa77
AyrFX <oleg@gbg.bg>
2016-06-21 23:13:30 +0300

    Added task 2 of Extension methods, delegates, lambda and LINQ homework

---
+++ 03. Extension-Methods-Delegates-Lambda-LINQ/02.IEnumerableExtensions/IEnumerableExtensions.cs
@@ -0,0 +1,65 @@
+﻿namespace IEnumerableExtensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public static class IEnumerableExtensions
+    {
+        public static T Sum<T>(this IEnumerable<T> collection) where T : IComparable, IConvertible
+        {
+            dynamic result = 0;
+            foreach (T element in collection)
+            {
+                result += element;
+            }
+            return result;
+        }
+
+        public static T Product<T>(this IEnumerable<T> collection) where T : IComparable, IConvertible
+        {
+            dynamic result = 1;
+            foreach (T element in collection)
+            {
+                result *= element;
+            }
+            return result;
+        }
+
+        public static T Min<T>(this IEnumerable<T> collection) where T : IComparable, IConvertible
+        {
+            T min = collection.First();
+            foreach (T element in collection)
+            {
+                if (element.CompareTo(min) < 0)
+                {
+                    min = element;
+                }
+            }
+            return min;
+        }
+
+        public static T Max<T>(this IEnumerable<T> collection) where T : IComparable, IConvertible
+        {
+            T max = collection.First();
+            foreach (T element in collection)
+            {
+                if (element.CompareTo(max) > 0)
+                {
+                    max = element;
+                }
+            }
+            return max;
+        }
+
+        public static T Average<T>(this IEnumerable<T> collection) where T : IComparable, IConvertible
+        {
+            dynamic result = 0;
+            foreach (T element in collection)
+            {
+                result += element;
+            }
+
+            return result/collection.Count();
+        }
+    }
+}
---
+++ 03. Extension-Methods-Delegates-Lambda-LINQ/02.IEnumerableExtensions/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace IEnumerableExtensions
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Startup
+    {
+        static void Main()
+        {
+            List<int> someCollection = new List<int>();
+            someCollection.Add(7);
+            someCollection.Add(3);
+            someCollection.Add(4);
+            someCollection.Add(12);
+            someCollection.Add(21);
+            someCollection.Add(16);
+            someCollection.Add(24);
+            Console.WriteLine("Sum: " + someCollection.Sum<int>());
+            Console.WriteLine("Product: " + someCollection.Product<int>());
+            Console.WriteLine("Min: " + someCollection.Min<int>());
+            Console.WriteLine("Max: " + someCollection.Max<int>());
+            Console.WriteLine("Average: " + someCollection.Average<int>());
+        }
+    }
+}
