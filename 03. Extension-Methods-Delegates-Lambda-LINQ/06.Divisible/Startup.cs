0f952dfcfa02b835d7b3207712846624e31ea0a9
AyrFX <oleg@gbg.bg>
2016-06-22 22:59:27 +0300

    Added task 6 of Extension methods, Delegates, Lambda & LINQ homework

---
+++ 03. Extension-Methods-Delegates-Lambda-LINQ/06.Divisible/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace Divisable
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbersArray = { 16, 7, 3, 12, 24, 4, 21, 18, 27, 28, 32, 63 };
+
+            var selectedWithMethods =
+                numbersArray
+                .Where(n => n % 7 == 0 && n % 3 == 0)
+                .Select(n => n);
+
+            var selecredWithLINQ =
+                from number in numbersArray
+                where number % 7 == 0 && number % 3 == 0
+                select number;
+
+            foreach (var number in selectedWithMethods)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine(new string('=', 20));
+
+            foreach (var number in selecredWithLINQ)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
