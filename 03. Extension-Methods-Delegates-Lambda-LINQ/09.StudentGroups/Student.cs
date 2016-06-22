7fb305e3e37bb9e20e937d6cf6604c3b5ab674c2
AyrFX <oleg@gbg.bg>
2016-06-22 23:11:29 +0300

    Added task 9 of Extension methods, delegates, lambda and LINQ homework

---
+++ 03. Extension-Methods-Delegates-Lambda-LINQ/09.StudentGroups/Student.cs
@@ -0,0 +1,35 @@
+﻿namespace StudentGroups
+{
+    using System.Collections.Generic;
+
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int FacultyNumber { get; set; }
+        public int GroupNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EMail { get; set; }
+        public List<int> Marks { get; set; }
+
+        public Student(string firstName, string lastName, int facultyNumber, int groupNumber, string phoneNumber, string email, List<int> marks)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.FacultyNumber = facultyNumber;
+            this.GroupNumber = groupNumber;
+            this.PhoneNumber = phoneNumber;
+            this.EMail = email;
+            this.Marks = marks;
+        }
+
+        public string ToString()
+        {
+            string result = this.FirstName + " " + this.LastName + ", faculty Number: " +
+                this.FacultyNumber + ", Group: " + this.GroupNumber + ", Phone: " +
+                this.PhoneNumber + ", e-mail: " + this.EMail + ", Marks: " +
+                string.Join(",", this.Marks);
+            return result;
+        }
+    }
+}
