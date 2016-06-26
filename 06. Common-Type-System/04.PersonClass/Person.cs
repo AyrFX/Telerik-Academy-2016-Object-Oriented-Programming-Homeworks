cad51fabb0732c77a2ee0e32c0e41c2c872b850b
AyrFX <oleg@gbg.bg>
2016-06-26 19:03:30 +0300

    Added task 4 of CTS Homework

---
+++ 06. Common-Type-System/04.PersonClass/Person.cs
@@ -0,0 +1,34 @@
+﻿namespace PersonClass
+{
+    public class Person
+    {
+        //Properties
+        public string Name { get; set; }
+        public byte? Age { get; set; }
+
+        //Constructors
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public Person(string name, byte? age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            if (this.Age == null)
+            {
+                return "Name: " + this.Name + "; Age:s unspecified";
+            }
+            else
+            {
+                return "Name: " + this.Name + "; Age: " + this.Age;
+            }
+        }
+    }
+}
---
+++ 06. Common-Type-System/04.PersonClass/Startup.cs
@@ -0,0 +1,21 @@
+﻿namespace PersonClass
+{
+    class Startup
+    {
+        static void Main()
+        {
+            Person[] personsArray =
+            {
+                new Person("Pesho"),
+                new Person("Gosho", 27),
+                new Person("Tosho"),
+                new Person("Mosho", 31)
+            };
+
+            foreach (var person in personsArray)
+            {
+                System.Console.WriteLine(person);
+            }
+        }
+    }
+}
