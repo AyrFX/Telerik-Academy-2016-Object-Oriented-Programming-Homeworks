a1c3ea346f84b6551353b18edc9d9ae257439464
AyrFX <oleg@gbg.bg>
2016-06-26 17:14:58 +0300

    Added task 3 of OOOP Principles Part 2 homework

---
+++ 05. OOP-Principles-Part-2/03.RangeException/InvalidRangeException.cs
@@ -0,0 +1,33 @@
+﻿namespace RangeException
+{
+    using System;
+
+    class InvalidRangeException<T> : ApplicationException
+    {
+        //Constructors
+        public InvalidRangeException(T rangeStart, T rangeEnd)
+            : base()
+        {
+            this.RangeStart = rangeStart;
+            this.RangeEnd = rangeEnd;
+        }
+
+        public InvalidRangeException(string message, T rangeStart, T rangeEnd)
+            : base(message)
+        {
+            this.RangeStart = rangeStart;
+            this.RangeEnd = rangeEnd;
+        }
+                
+        public InvalidRangeException(string message, Exception inner, T rangeStart, T rangeEnd)
+            : base(message, inner)
+        {
+            this.RangeStart = rangeStart;
+            this.RangeEnd = rangeEnd;
+        }
+
+        //Properties
+        public T RangeStart { get; private set; }
+        public T RangeEnd { get; private set; }
+    }
+}
---
+++ 05. OOP-Principles-Part-2/03.RangeException/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace RangeException
+{
+    using System;
+
+    class Startup
+    {
+        static void Main()
+        {
+            int someInteger = -20;
+            try
+            {
+                if (someInteger < 1 || someInteger > 100)
+                {
+                    throw new InvalidRangeException<int>("The number is out of the range", 1, 100);
+                }
+                else Console.WriteLine(someInteger);
+            }
+            catch (InvalidRangeException<int> e)
+            {
+                Console.WriteLine("{0}. Available range is between {1} and {2}", e.Message, e.RangeStart, e.RangeEnd);
+            }
+
+            var someDateTime = new DateTime(2016, 6, 24);
+            try
+            {
+                var startDate = new DateTime(1980, 1, 1);
+                var endDate = new DateTime(2013, 12, 31);
+                if (startDate > someDateTime || someDateTime > endDate)
+                {
+                    throw new InvalidRangeException<DateTime>("The date is out of the range", startDate, endDate);
+                }
+                else
+                {
+                    Console.WriteLine(someDateTime);
+                }
+            }
+            catch (InvalidRangeException<DateTime> e)
+            {
+                Console.WriteLine("{0}. Available range is between {1} and {2}", e.Message, e.RangeStart, e.RangeEnd);
+            }
+        }
+    }
+}
